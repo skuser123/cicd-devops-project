6ff071868b7164f427e363d27119e6b1ef8954c7
Sabita <skuser846@gmail.com>
2026-05-10 23:15:39 +0530

    litile change for testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello DevOps Engineer! CI/CD Pipeline successfully running! skuser</h1>"
+    return "<h1>Hello DevOps Engineer! CI/CD Pipeline successfully running!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
